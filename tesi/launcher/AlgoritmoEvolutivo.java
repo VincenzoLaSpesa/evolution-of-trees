5a6fe3a033f843842e68597e6a94da6b8f561ba1
darshan_tweet <vincenzolaspesa@gmail.com>
2014-04-16 00:33:52 +0200

    Commit prima del refractoring

--- tesi/launcher/AlgoritmoEvolutivo.java
+++ tesi/launcher/AlgoritmoEvolutivo.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 import java.util.logging.Logger;
 
 import tesi.controllers.GAIT_abstract;
+import tesi.controllers.GAIT_simple;
 import tesi.controllers.GeneticOperators;
 import tesi.controllers.TreeEvaluator;
 import tesi.interfaces.CromosomaDecorator;
@@ -11,6 +12,7 @@ import tesi.models.Cromosoma;
 import tesi.models.Dataset;
 import tesi.util.SingletonGenerator;
 import tesi.util.logging.GlobalLogger;
+import weka.classifiers.trees.J48;
 import weka.core.Instances;
 
 /**
@@ -139,5 +141,75 @@ public abstract class AlgoritmoEvolutivo {
 		}
 
 	}
+	
+	public CromosomaDecorator startevolution(GAIT_abstract algoritmo) throws Exception {
+		double prestazioni_gait;
+		double prestazioni_j48 = -1;
+		double peso_gait;
+		double peso_J48w = -1;
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(String.format("Il training set è composto da \t%d elementi\n", trainingset.numInstances()));
+		sb.append(String.format("Il test set è composto da \t%d elementi\n", testset.numInstances()));
+		sb.append(String.format("Lo scoring set è composto da \t%d elementi\n", scoringset.numInstances()));
+		sb.append(String.format("Evolvo per %d generazioni con una popolazione fissa di %d elementi\n\n",
+				this.numerogenerazioni, this.popolazione_iniziale_size));
+		logger.info(sb.toString());
+		trainingset.setClassIndex(trainingset.numAttributes() - 1);
+		testset.setClassIndex(testset.numAttributes() - 1);
+		for (int i = 0; i < popolazione_iniziale_size; i++) {
+			Instances data = new Instances(trainingset, i * campioni_per_albero, campioni_per_albero);
+			data.setClassIndex(trainingset.numAttributes() - 1);
+			J48 j48 = new J48();
+			j48.setBinarySplits(true);
+			j48.buildClassifier(data);
+			Cromosoma c = Cromosoma.loadFromJ48(j48);
+			popolazione_iniziale.add(c);
+		}
+		
+		J48 j48 = new J48();
+		sb = new StringBuilder();
+		sb.append(String.format("La popolazione iniziale è generata con J48, un porting in Java di C4.5"));
+		sb.append(String.format(j48.getTechnicalInformation().toBibTex() + "\n"));
+		logger.fine(sb.toString());
+		ecosistema = algoritmo;
+		esemplare = ecosistema.GAIT(popolazione_iniziale, numerogenerazioni, mutante);
+		te = new TreeEvaluator(esemplare, testset, nclassi);
+		te.evaluate();
+		prestazioni_gait = te.getPrestazioni();
+		peso_gait = esemplare.getComplessita();
+		cd = new CromosomaDecorator(esemplare);
+		cd.caricaColonne(trainingset);
+		sb = new StringBuilder();
+		sb.append("L'esemplare migliore dopo n generazioni è il seguente:\n");
+		sb.append(cd.getGraph());
+		sb.append("\n");
+		sb.append(esemplare.toYaml());
+		sb.append(String.format("\ne ha peso: %.1f \n", peso_gait));
+		sb.append("le prestazioni dell'esemplare migliore calcolate sul testset sono:");
+		sb.append(String.format("p=\t%f\n", prestazioni_gait));
+		sb.append(te.getConfusionasFloatString());
+		sb.append("\n");
+		logger.info(sb.toString());
+		//
+		if (eseguiwhole) {
+			j48.setBinarySplits(true);
+			j48.buildClassifier(trainingset);
+			Cromosoma whole = Cromosoma.loadFromJ48(j48);
+			te = new TreeEvaluator(whole, testset, nclassi);
+			te.evaluate();
+			sb = new StringBuilder();
+			sb.append("Le prestazioni dell'albero generato sull'intero trainingset e calcolate sul testset (wholetraining) sono:");
+			prestazioni_j48 = te.getPrestazioni();
+			peso_J48w = whole.getComplessita();
+			sb.append(String.format("%f\n", prestazioni_j48));
+			sb.append("\n");
+			sb.append(te.getConfusionasFloatString());
+			logger.info(sb.toString());
+		}
+		System.out.printf("§§\t%f\t%f\t%.1f\t%.1f\n", prestazioni_gait, prestazioni_j48, peso_gait, peso_J48w);
+		return cd;
+	}
+
 
 }
